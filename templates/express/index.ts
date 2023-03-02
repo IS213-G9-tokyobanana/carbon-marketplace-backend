b0fad8dddf69ebbb7e60397fa4a995b26cf1107a
nicamanas <xyukimiku@gmail.com>
2023-03-03 01:00:05 +0800

    postgresql database container created
    includes express+prisma integration, refer to src/postgrestest.ts for example data add and query method creation
    
    refer to templates/express/index.ts on how to create routes to get data

--- templates/express/index.ts
+++ templates/express/index.ts
@@ -1,5 +1,8 @@
 // TODO
-const express = require("express")
+import { PrismaClient } from ".prisma/client"
+import express from "express"
+
+const prisma = new PrismaClient()
 const app = express()
 const port = 5000
 
@@ -9,4 +12,10 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
+})
+
+// route to retrieve users from database
+app.get("/users", async (req, res) => {
+	const users = await prisma.user.findMany()
+	res.json(users)
 })
