f3351c6594ff914ee17a8b3c4b0c2728804d8bbe
nicamanas <xyukimiku@gmail.com>
2023-03-06 19:27:12 +0800

    feat: add express-postgres-prisma template (#1)

--- templates/express/index.ts
+++ templates/express/index.ts
@@ -1,1 +1,21 @@
 // TODO
+import { PrismaClient } from ".prisma/client"
+import express from "express"
+
+const prisma = new PrismaClient()
+const app = express()
+const port = 5000
+
+app.get("/", (req, res) => {
+	res.send("Hello World!")
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
+
+// route to retrieve users from database
+app.get("/users", async (req, res) => {
+	const users = await prisma.user.findMany()
+	res.json(users)
+})
