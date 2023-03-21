bf561cb0524e3140e5255794349f3e66d82fd768
Zong Wei Eng <engzongwei@hotmail.com>
2023-03-21 16:27:26 +0800

    Create scheduler.py

---
+++ scheduler/scheduler.py
@@ -0,0 +1,14 @@
+from config import (
+    RMQHOSTNAME,
+    RMQPORT,
+    RMQUSERNAME,
+    RMQPASSWORD,
+    SCHEDULER_EXCHANGE_NAME,
+    PUBLISHED_TASK_EXECUTE_ROUTING_KEY,
+)
+
+def checkType(data):
+    print(data)
+
+if __name__ == "__main__":
+    pass
