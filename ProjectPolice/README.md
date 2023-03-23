7b9d1265f0a4b94dc027cf31e93d3b2a1827bde1
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-03-24 03:16:24 +0800

    refactor: changed to rabbitmq async method Created a config folder for folder cleanliness Fixed code according to comments made by reviewer

--- ProjectPolice/README.md
+++ ProjectPolice/README.md
@@ -12,22 +12,23 @@ This is the Project Police complex microservice. It is a Flask microservice that
 1. Python 3.11 - https://www.python.org/downloads/
 2. Poetry 1.4.0 - https://python-poetry.org/docs/#installation
 3. Docker 20.10.8 - https://docs.docker.com/get-docker/
+4. Have a live RabbitMQ instance running. Environment file should be configured to attach to the RabbitMQ instance.
 
 ### Initial Setup
 
- 1. Clone the repository
+1.  Clone the repository
 
- ```bash
- git clone
- ```
+```bash
+git clone
+```
 
- 2. Copy and rename the `.env.example` file to `.env`. Please remember to update the environment variables accordingly.
+2.  Copy and rename the `.env.example` file to `.env`. Please remember to update the environment variables accordingly.
 
- ```bash
- cp .env.example .env
- ```
+```bash
+cp .env.example .env
+```
 
- 3. Change configuration in `config.py` file based on whether it is production or development environment.
+3.  Change configuration in `config.py` file based on whether it is production or development environment.
 
 ### Running the app
 
@@ -47,19 +48,23 @@ docker build -t projectpolice .
 docker run -d -p 5000:5000 projectpolice
 ```
 
-4. Create a virtual environment
+### Local Development
+
+For local development, configure the rabbitMQ hostname variables in the `.env` file to 'localhost'. Then, continue with the following steps after running the docker container.
+
+1. Create a virtual environment
 
 ```
 poetry shell
 ```
 
-5. Install dependencies
+2. Install dependencies
 
 ```
 poetry install
 ```
 
-6. Run the app
+3. Run the app
 
 ```
 python3 checkAmqp.py
--- ProjectPolice/checkAmqp.py
+++ ProjectPolice/checkAmqp.py
@@ -1,92 +1,87 @@
 import json
 import pika
-from config import (
+import logging
+from config.config import (
+    EXCHANGE_NAME,
+    TASK_EXECUTE_BINDING_KEY,
+    QUEUE_NAME,
     RMQHOSTNAME,
     RMQPORT,
     RMQUSERNAME,
     RMQPASSWORD,
-    EXCHANGE_NAME,
-    TASK_EXECUTE_ROUTING_KEY,
 )
 import police
 
-# Connect to RabbitMQ
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        host=RMQHOSTNAME,
-        port=RMQPORT,
-        heartbeat=3600,
-        blocked_connection_timeout=3600,
-        credentials=pika.PlainCredentials(RMQUSERNAME, RMQPASSWORD),
+# Global variable
+channel = None
+
+
+# Function is called when connection is open, and channel is open
+def on_open(connection: pika.SelectConnection):
+    connection.channel(on_open_callback=on_channel_open)
+
+
+# Function is called when channel is open, and queue is declared
+def on_channel_open(new_channel: pika.channel.Channel):
+    global channel
+    channel = new_channel
+    # Create a task_triggered_topic queue
+    channel.queue_declare(QUEUE_NAME, durable=True, callback=on_queue_declared)
+    # Bind the queue to the exchange
+    channel.queue_bind(
+        exchange=EXCHANGE_NAME,
+        queue=QUEUE_NAME,
+        routing_key=TASK_EXECUTE_BINDING_KEY,
     )
-)
-
-# Create a connection channel
-channel = connection.channel()
-
-# Create a task_triggered_topic queue
-channel.queue_declare("task_triggered_topic", durable=True)
-channel.queue_bind(
-    exchange=EXCHANGE_NAME,
-    queue="task_triggered_topic",
-    routing_key=TASK_EXECUTE_ROUTING_KEY,
-)
 
 
-def check_setup():
-    global connection, channel, RMQHOSTNAME, RMQPORT
-    if not is_connection_open(connection):
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=RMQHOSTNAME,
-                port=RMQPORT,
-                heartbeat=3600,
-                blocked_connection_timeout=3600,
-            )
-        )
-    if channel.is_closed:
-        channel = connection.channel()
-        channel.exchange_declare(
-            exchange=EXCHANGE_NAME, exchange_type="topic", durable=True
-        )
+# Function is called when queue is declared, and basic_consume is called
+def on_queue_declared(frame: pika.frame.Method):
+    channel.basic_consume(QUEUE_NAME, callback, auto_ack=False)
 
 
-def is_connection_open(connection):
-    try:
-        connection.process_data_events()
-        return True
-    except pika.exceptions.AMQPError as e:
-        print("AMQP Error:", e)
-        print("...creating a new connection.")
-        return False
-
-
-def receiveMsg():
-    check_setup()
-    channel.basic_consume(
-        queue="task_triggered_topic",
-        on_message_callback=callback,
-    )
-    channel.start_consuming()
-
-
+# Function is called when message is received
 def callback(channel, method, properties, body):
-    processTrigger(body)
-
-
-def processTrigger(message):
     try:
-        data = json.loads(message)
-        police.checkType(data)
-    except Exception as e:
-        print("--NOT JSON:", e)
-        print("--DATA:", data)
+        print("Received message:", body)
+        data = json.loads(body)
+        check_message(data)
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as err:
+        logging.exception("Error processing message: %s", err)
+
+
+# Function is called when message is received, and message type is checked
+def check_message(data: dict):
+    if data["type"] == "upcoming":
+        police.publish_to_notifier(data, channel)
+    elif data["type"] == "penalise":
+        police.send_to_projectms(data)
+    elif data["type"] == "overdue":
+        # Trigger Temporal.io workflow
+        return
+    else:
+        print("Invalid message type")
 
 
 if __name__ == "__main__":
     print(
-        ": monitoring routing key '{}' in exchange '{}' ...".format(
-            "events.*.*.task.execute", EXCHANGE_NAME
-        )
+        f"monitoring the exchange {EXCHANGE_NAME} on binding key {TASK_EXECUTE_BINDING_KEY} ..."
     )
-    receiveMsg()
+    # Define connection parameters
+    parameters = pika.ConnectionParameters(
+        host=RMQHOSTNAME,
+        port=RMQPORT,
+        heartbeat=3600,
+        credentials=pika.PlainCredentials(RMQUSERNAME, RMQPASSWORD),
+        blocked_connection_timeout=3600,
+    )
+    # Connect to RabbitMQ
+    connection = pika.SelectConnection(parameters=parameters, on_open_callback=on_open)
+    # Start the IOLoop to allow the SelectConnection to operate
+    try:
+        connection.ioloop.start()
+    except KeyboardInterrupt:
+        # Gracefully close the connection
+        connection.close()
+        connection.ioloop.start()
