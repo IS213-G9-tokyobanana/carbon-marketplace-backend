69eb02051d444063467dbcc34fb8d6bfd6000371
Jaylin Lee <68802506+Jaylin0312@users.noreply.github.com>
2023-03-25 02:19:51 +0800

    feat: basic project police (#10)
    * feat: auto create exchange on docker compose up
    
    * feat: added readme
    
    * feat: added config file to read env variables
    
    * feat: created task_triggered_topic queue
    This queue listens to exchange 'topic_exchange'
    
    * fix: fix callback function
    
    * feat: notify verifier + post request to project ms
    
    * style: format file
    
    * refactor: rename function name
    
    * feat: added poetry and docker file
    
    * chore: added readme file
    
    * chore: update readme with env example
    
    * fix: fixed typo in readme
    
    * fix: change payload for penalise scenario
    
    * feat: payments microservice (#8)
    
    * PaymentIntent Creation and update in database
    
    - PaymentIntent is created through POST request of payment amount and currency in Stripe
    - PaymentIntent is logged in MongoDB database in docker container
    
    todo at this point:
    - clean up code and feedback messages
    - make mongodb create the database and collection on docker-compose up
    
    * Dockerfile and docker-compose up functionality
    
    Full containerization of payment service and related database container
    
    * Update README.md
    
    * removed prisma and mongo-express
    
    * binded docker volume to mongodb and defined docker network for payment containers
    
    * Appended additional fields to dbtransaction + renamed request
    
    * break up connect to database and create collection functions
    
    * fixed package json linting
    
    * fixed return values
    
    * fixed dockerfile npm run start
    
    * revert database functions split
    
    * update service container port number
    
    * Fixed error response
    
    Fixed error response and split paymentintent creation, as well as db transaction into separate try catch blocks
    
    * Update README.md
    
    * Create .env.example
    
    included example .env file for reference
    
    * Create .dockerignore
    
    * changed project_id to milestone_id
    
    * feat: basic user microservice (#9)
    
    * feat: init gin
    
    * feat: setup db & rest helper funcs
    
    * feat: models for user & offset
    
    * feat: user controller
    
    * feat: init swagger
    
    * feat: readme and format
    
    * feat: add postman tests
    
    * chore: update readme
    
    * chore: update readme with .env explanation
    
    * fix: healthcheck routes
    
    * fix: removed unuses init file
    
    * refactor: changed to rabbitmq async method
    Created a config folder for folder cleanliness
    Fixed code according to comments made by reviewer
    
    * feat: added config file to read env variables
    
    * feat: created task_triggered_topic queue
    This queue listens to exchange 'topic_exchange'
    
    * fix: fix callback function
    
    * feat: notify verifier + post request to project ms
    
    * style: format file
    
    * refactor: rename function name
    
    * feat: added poetry and docker file
    
    * chore: added readme file
    
    * chore: update readme with env example
    
    * fix: fixed typo in readme
    
    * fix: change payload for penalise scenario
    
    * fix: removed unuses init file
    
    * refactor: changed to rabbitmq async method
    Created a config folder for folder cleanliness
    Fixed code according to comments made by reviewer
    
    * fix: changed to match other ms
    
    * feat: added routing key to config file
    
    * feat: added exceptions
    
    * feat: removed unsued ENV
    
    * fix: removed unnecessary code
    
    * feat: put project ms url in config
    
    * fix: changed URL and exception handling
    
    ---------
    
    Co-authored-by: nicamanas <xyukimiku@gmail.com>
    Co-authored-by: David Lin <13061926+DavidLHW@users.noreply.github.com>

---
+++ ProjectPolice/checkAmqp.py
@@ -0,0 +1,90 @@
+import json
+import pika
+import logging
+from config.config import (
+    EXCHANGE_NAME,
+    TASK_EXECUTE_BINDING_KEY,
+    QUEUE_NAME,
+    RMQHOSTNAME,
+    RMQPORT,
+    RMQUSERNAME,
+    RMQPASSWORD,
+)
+import police
+
+# Global variable
+channel = None
+
+
+# Function is called when connection is open, and channel is open
+def on_open(connection: pika.SelectConnection):
+    connection.channel(on_open_callback=on_channel_open)
+
+
+# Function is called when channel is open, and queue is declared
+def on_channel_open(new_channel: pika.channel.Channel):
+    global channel
+    channel = new_channel
+    # Create a task_triggered_topic queue
+    channel.queue_declare(QUEUE_NAME, durable=True, callback=on_queue_declared)
+    # Bind the queue to the exchange
+    channel.queue_bind(
+        exchange=EXCHANGE_NAME,
+        queue=QUEUE_NAME,
+        routing_key=TASK_EXECUTE_BINDING_KEY,
+    )
+
+
+# Function is called when queue is declared, and basic_consume is called
+def on_queue_declared(frame: pika.frame.Method):
+    channel.basic_consume(QUEUE_NAME, callback, auto_ack=False)
+
+
+# Function is called when message is received
+def callback(channel, method, properties, body):
+    try:
+        print("Received message:", body)
+        data = json.loads(body)
+        check_message(data)
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as err:
+        logging.exception("Error processing message: %s", err)
+
+
+# Function is called when message is received, and message type is checked
+def check_message(data: dict):
+    try:
+        if data["type"] == "upcoming":
+            police.publish_to_notifier(data, channel)
+        elif data["type"] == "penalise":
+            police.send_to_projectms(data)
+        elif data["type"] == "overdue":
+            # Trigger Temporal.io workflow
+            return
+        else:
+            print("Invalid message type")
+    except Exception as err:
+        logging.exception("Error processing message: %s", err)
+
+
+if __name__ == "__main__":
+    print(
+        f"monitoring the exchange {EXCHANGE_NAME} on binding key {TASK_EXECUTE_BINDING_KEY} ..."
+    )
+    # Define connection parameters
+    parameters = pika.ConnectionParameters(
+        host=RMQHOSTNAME,
+        port=RMQPORT,
+        heartbeat=3600,
+        credentials=pika.PlainCredentials(RMQUSERNAME, RMQPASSWORD),
+        blocked_connection_timeout=3600,
+    )
+    # Connect to RabbitMQ
+    connection = pika.SelectConnection(parameters=parameters, on_open_callback=on_open)
+    # Start the IOLoop to allow the SelectConnection to operate
+    try:
+        connection.ioloop.start()
+    except KeyboardInterrupt:
+        # Gracefully close the connection
+        connection.close()
+        connection.ioloop.start()
---
+++ ProjectPolice/police.py
@@ -0,0 +1,44 @@
+import requests
+import pika
+import json
+from config.config import EXCHANGE_NAME, POLICE_NOTIFY_ROUTING_KEY, PROJECT_STATUS_URL
+
+def format_url(pid, mid):
+    return PROJECT_STATUS_URL.format(project_id=pid, milestone_id=mid)
+
+# Function to format message before sending to Notifier / Project Microservice
+def format_message(resource_id, type, data):
+    new_data = {k:v for k,v in data.items() if k != "task_id"}
+    return json.dumps({"resource_id": resource_id, "type": type, "data": new_data})
+
+
+# Function to send message to Notifier
+def publish_to_notifier(message, channel):
+    milestoneid = message["data"]["milestone_id"]
+    type = "milestone.upcoming"
+    payload = format_message(milestoneid, type, message["data"])
+    channel.basic_publish(
+        exchange=EXCHANGE_NAME,
+        routing_key=POLICE_NOTIFY_ROUTING_KEY,
+        body=payload,
+        properties=pika.BasicProperties(delivery_mode=2),
+    )
+
+
+# Function to send message to Project Microservice
+def send_to_projectms(message):
+    milestone_id = message["data"]["milestone_id"]
+    project_id = message["data"]["project_id"]
+    payload = format_message(milestone_id, project_id, message["data"])
+    url = format_url(project_id, milestone_id)
+
+    # Send request to Project Microservice
+    try:
+        result = requests.patch(url, json=payload)
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        result = {
+            "code": 500,
+            "message": "invocation of service fails: " + url + ". " + str(err),
+        }
+    return result
