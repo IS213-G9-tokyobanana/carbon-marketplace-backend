120f96df0404b567be67f77e4107761dd73fbfe3
Zong Wei Eng <engzongwei@hotmail.com>
2023-03-24 18:05:58 +0800

    Update scheduler.py

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -4,17 +4,17 @@ from datetime import datetime, timedelta
 
 # TODO:
 # [x] 1. CheckType of each message and call the appropriate function
-# [] 2. Upcoming Milestone Funct
+# [x] 2. Upcoming Milestone Funct
 #   [x] 2.1. addMilestoneJob()
 #   [x] 2.2. addProject()
-#   [] 2.3. milestoneRewarded()
-# [] 3. Overdue Milestone Funct
+#   [x] 2.3. milestoneRewarded()
+# [x] 3. Overdue Milestone Funct
 #   [x] 3.1. addMilestoneJob()
 #   [x] 3.2. addProject()
-#   [] 3.3. milestoneRewarded()
-# [] 4. Reserve Offset Funct
-#   [] 4.1. newOffsetTrack()
-#   [] 4.2. removeOffsetTrack()
+#   [x] 3.3. milestoneRewarded()
+# [x] 4. Reserve Offset Funct
+#   [x] 4.1. newOffsetTrack()
+#   [x] 4.2. removeOffsetTrack()
 # [] 5. Re-Publish scheduler task Funct
 #   [] 5.1. publishTask()
 
@@ -29,10 +29,9 @@ def checkType(msg):
         addMilestoneJob(milestoneId, projId, milestone)
     elif msg['type'] == 'project.verify':
         # New project has been created and all milestones need to be added
-        projId = msg['data']['project_id']
-        milestoneId = msg['resource_id']
+        projId = msg['resource_id']
         milestone = msg['data']['milestones']
-        addProject(milestoneId, projId, milestone)
+        addProject(projId, milestone)
     elif msg['type'] == 'offsets.reserve':
         # Start tracking the TTL for the offset (1hr)
         payment_id = msg['resource_id']
@@ -43,32 +42,46 @@ def checkType(msg):
         # Check if in Cron to see if need to remove.
         payment_id = msg['resource_id']
         removeOffsetTrack(payment_id)
-    elif msg['type'] == 'milestone.rewarded':
+    elif msg['type'] == 'milestone.reward':
         # Milestone has been rewarded, need to remove from cron
         milestone_id = msg['resource_id']
-        milestoneRewarded(milestone_id)
+        projId = msg['data']['project_id']
+        milestoneRewarded(milestone_id, projId)
     elif msg['type'] == 'task.add':
         # Re-publish the task to the exchange
-        publishTask()
+        # publishTask()
+        print(msg)
 
 
 # Function that looks for the Offset tracking and removes it from Cron
 def removeOffsetTrack(payment_id):
-    pass
+    print('in scheduler removeOffsetTrack')
+    cron = CronTab(user=True)
+    cron.remove_all(comment=f'offset_{payment_id}')
+    cron.write()
 
 # Function that creates a new job to track TTL
 def newOffsetTrack(payment_id, created_at):
-    pass
+    print('in scheduler newOffsetTrack')
+    cron = CronTab(user=True)
+    job  = cron.new(command='python ./publisher.py', comment=f'offset_{payment_id}')
+    job.setall(datetime.fromisoformat(created_at) + timedelta(hours=1))
+    cron.write()
 
 # Function that is called when milestone have been rewarded
-def milestoneRewarded(milestone_id):
+def milestoneRewarded(milestone_id, project_id):
     # Need to check 2 conditions
     # 1. If upcoming and overdue both still in Cron
     # 2. If only overdue in Cron
-    pass
+    print('in scheduler milestoneRewarded')
+    cron = CronTab(user=True)
+    cron.remove_all(comment=f'upcoming_{project_id}_{milestone_id}')
+    cron.write()
+    cron.remove_all(comment=f'overdue_{project_id}_{milestone_id}')
+    cron.write()
 
 # Function that is called when there is a new project. Will call addMilestoneJob() to add all milestones
-def addProject(milestone_id, project_id, milestones):
+def addProject(project_id, milestones):
     print('in scheduler addProject')
     for milestone in milestones:
         milestone_id = milestone['milestone_id']
@@ -83,26 +96,13 @@ def addMilestoneJob(milestone_id, project_id, milestone):
     year, month, day = date.split('-')
     hour, minute, second = time.split(':')
     cron = CronTab(user=True)
-    job  = cron.new(command='python publisher', comment=f'upcoming_{project_id}_{milestone_id}')
+    job  = cron.new(command='python ./publisher.py', comment=f'upcoming_{project_id}_{milestone_id}')
     job.setall(datetime(int(year), int(month), int(day), int(hour), int(minute), int(second)) - timedelta(30))
     cron.write()
 
-    job  = cron.new(command='python publisher', comment=f'overdue_{project_id}_{milestone_id}')
+    job  = cron.new(command='python ./publisher.py', comment=f'overdue_{project_id}_{milestone_id}')
     job.setall(datetime(int(year), int(month), int(day), int(hour), int(minute), int(second)) + timedelta(1))
     cron.write()    
-
-def test_cron():
-    print('in scheduler test cron')
-    cron = CronTab(user=True)
-    job  = cron.new(command='echo hello_world', comment='test1')
-    job.minute.every(3)
-    cron.write()
-    job  = cron.new(command='echo hello_world2', comment='test2')
-    job.minute.every(5)
-    cron.write()
-    cron.remove_all(comment='test1')
-    cron.write()
-    print('in scheduler after cron')
 
 if __name__ == "__main__":
     pass
