24e8309e9261327b54521a164daca9b16b01c313
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-03-24 23:29:41 +0800

    feat: put project ms url in config

--- ProjectPolice/config/config.py
+++ ProjectPolice/config/config.py
@@ -10,6 +10,7 @@ EXCHANGE_NAME = "topic_exchange"
 TASK_EXECUTE_BINDING_KEY="events.*.*.task.execute"
 POLICE_NOTIFY_ROUTING_KEY="events.police.notify.milestone.upcoming"
 QUEUE_NAME="task_triggered_topic_queue"
-PROJECT_MS_URL = "http://localhost:5000/project/{project_id}/milestone/{milestone_id}/penalise"
+PROJECT_MS = os.getenv('projectms')
+PROJECT_MS_URL = "{PROJECT_MS}/project/{project_id}/milestone/{milestone_id}/penalise"
 
 
