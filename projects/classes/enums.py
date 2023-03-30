2e3a4f2337608ee1d223aba7252028715d37d5b7
zhiweit <theanzhiwei@gmail.com>
2023-03-30 14:55:17 +0800

    chore: updated milestone add POST endpoint and updated message type field according to scheduler ms

--- projects/classes/enums.py
+++ projects/classes/enums.py
@@ -17,3 +17,13 @@ class MilestoneStatus(StrEnum):
     def values(cls) -> list:
         return [v.value for v in cls._value2member_map_.values()]
 
+class AmqpPayloadType(StrEnum):
+    #Payload 'type' field
+    OFFSETS_COMMIT = "offsets.commit"
+    OFFSETS_RESERVE = "offsets.reserve"
+    OFFSETS_ROLLBACK = "offsets.rollback"
+    PROJECT_CREATE = "project.create"
+    PROJECT_VERIFY = "project.verify"
+    MILESTONE_ADD = "milestone.add"
+    MILESTONE_REWARD = "milestone.reward"
+    MILESTONE_PENALISE = "milestone.penalise"
