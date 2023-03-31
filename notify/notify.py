13cbb2abb8f37407dcb8a84d613420cf602f9c5f
ZheTao <zacharytao99@gmail.com>
2023-03-31 20:42:09 +0800

    updated refactored code

--- notify/notify.py
+++ notify/notify.py
@@ -16,82 +16,112 @@ from amqp_setup import (
     message,
     channel_consume,
     message_buyer,
-    message_seller
+    message_seller, SUBJECT
 )
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def process_message(data, queue_name, SUBJECT):
-    retrieved_message = data
-    subject_retrieved = SUBJECT
-    retrieved_role = ""
-    retrieved_user_email, retrieved_role = retrieve_users_information()
-    print(retrieved_role)
-    result_message = format_message(queue_name, retrieved_message, retrieved_role)
-    send_email_to_user(retrieved_user_email, result_message, subject_retrieved)
-
-#buyer_id 
-#seller_id 
-def format_message(queue_name, retrieved_message, retrieved_role):
-    print(queue_name)
-
-    if queue_name == "ratings_reward" or queue_name == "ratings_penalise":
-        format_resource_id = retrieved_message.get("resource_id",{})
-        format_milestone_id = retrieved_message.get("data").get("milestones", {}).get("id", {})
-    elif queue_name == "milestone_upcoming":
-        format_resource_id = retrieved_message.get("data").get("project_id", {})
-        format_milestone_id = retrieved_message.get("data").get("milestone_id", {})
-    else:
-        format_resource_id = retrieved_message.get("resource_id",{})
-        format_milestone = retrieved_message.get("data").get("milestones", {})[0]
-        format_milestone_id = format_milestone.get("id", {})
-
-    format_buyer_id = retrieved_message.get("data").get("buyer_id", {})
-    format_seller_id = retrieved_message.get("data").get("seller_id",{})
-
-    for dict_queue_name in QUEUES.items():
-        if queue_name == dict_queue_name:
-            message_retrieved = QUEUES[queue_name][message]
-            new_message_retrieved = message_retrieved.format(project_id=format_resource_id, milestone_id=format_milestone_id)
-            if new_message_retrieved is None:
-                if retrieved_role == "buyer":
-                    message_retrieved = QUEUES[queue_name][message_buyer]
-                    new_message_retrieved = message_retrieved.format(buyer_id=format_buyer_id)
-                elif retrieved_role == "seller":
-                    message_retrieved = QUEUES[queue_name][message_seller]
-                    new_message_retrieved = message_retrieved.format(seller_id=format_seller_id)
-            else:
-                return new_message_retrieved
-        
-# communicate with the user microservice to retrieve users information
-def retrieve_users_information():
+def callback(channel, method, properties, body):
     try:
-        response = requests.get(MS_BASE_URL + "/users")
+        data = json.loads(body)
+        queue_name = "_".join(method.routing_key.split(".")[-2:])
+        print(queue_name)
+        email_subject = QUEUES[queue_name][SUBJECT]
+        # this is the data retrieved from the publisher
+        process_message(data, queue_name, email_subject)
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as err:
+        logging.exception("Error processing message: %s", err)
+
+def process_message(data, queue_name, SUBJECT):
+    if queue_name == "payment_success":
+        buyer_id = data.get("data").get("buyer_id", {})
+        seller_id = data.get("data").get("seller_id", {})
+
+        retrieved_email_buyer, retrieved_role_buyer = retrieve_user_email(buyer_id)
+        result_message = format_message(data, queue_name, retrieved_role_buyer)
+        send_email_to_user(retrieved_email_buyer, result_message, SUBJECT)
+
+        retrieved_email_seller, retrieved_role_seller = retrieve_user_email(seller_id)
+        result_message = format_message(data, queue_name, retrieved_role_seller)
+        send_email_to_user(retrieved_email_seller, result_message, SUBJECT)
+    elif queue_name == "payment_failed" or queue_name == "rollbackpayment_failed":
+        buyer_id = data.get("data").get("buyer_id", {})
+        retrieved_email_buyer, retrieved_role_buyer = retrieve_user_email(buyer_id)
+        result_message = format_message(data, queue_name, retrieved_role_buyer)
+        send_email_to_user(retrieved_email_buyer, result_message, SUBJECT)
+    else:
+        owner_id = data.get("data").get("owner_id", {})
+        retrieved_email, retrieved_role = retrieve_user_email(owner_id)
+        result_message = format_message(data, queue_name, retrieved_role)
+        send_email_to_user(retrieved_email, result_message, SUBJECT)
+ 
+# communicate with the user microservice to retrieve users information
+def retrieve_user_email(id):
+    try:
+        response = requests.get(MS_BASE_URL + id)
         response.raise_for_status()
+        #handles 404,403 and 500 error
     except requests.exceptions.HTTPError as e:
         status_code = e.response.status_code
         print("this is the status_code retrieved from HTTP Error:" + status_code)
-
+        if status_code == 404:
+            print("User not found")
     data_object = response.json()
-    role = data_object.get("data", {}).get("role")
-    if role == "buyer":
-        user_email_detail = "ownagersg+test@gmail.com"
-    elif role == "seller":
-        user_email_detail = "ownagersg+test1@gmail.com"
+    user_email = data_object.get("data", {}).get("email")
+    retrieved_role = data_object.get("data").get("type", {})
+    return (user_email, retrieved_role)
+
+def format_message(data, queue_name, retrieved_role):
+    if queue_name == "ratings_reward" or queue_name == "ratings_penalise":
+        format_resource_id = data.get("resource_id",{})
+        format_milestone_id = data.get("data").get("milestones", {}).get("id", {})
+    elif queue_name == "milestone_upcoming":
+        format_resource_id = data.get("data").get("project_id", {})
+        format_milestone_id = data.get("data").get("milestone_id", {})
+    elif queue_name == "payment_success":
+        format_buyer_id = data.get("data").get("buyer_id", {})
+        format_seller_id = data.get("data").get("seller_id",{})
+    elif queue_name == "payment_failed" or queue_name == "rollbackpayment_failed":
+        format_buyer_id = data.get("data").get("buyer_id", {})
     else:
-        user_email_detail = "ownagersg+test2@gmail.com"
-    return (user_email_detail,role)
+        format_resource_id = data.get("resource_id",{})
+        format_milestone = data.get("data").get("milestones", {})[0]
+        format_milestone_id = format_milestone.get("id", {})
 
+    if queue_name in QUEUES:
+        queue_data = QUEUES[queue_name]
+    message_retrieved = queue_data.get(message)
 
+    if message_retrieved is not None:
+        new_message_retrieved = message_retrieved.format(
+            project_id=format_resource_id,
+            milestone_id=format_milestone_id
+        )
+        if new_message_retrieved is not None:
+            return new_message_retrieved
+    elif retrieved_role == "buyer":
+        message_retrieved = queue_data.get(message_buyer)
+        if message_retrieved is not None:
+            return message_retrieved.format(buyer_id=format_buyer_id)
+    elif retrieved_role == "seller":
+        message_retrieved = queue_data.get(message_seller)
+        if message_retrieved is not None:
+            return message_retrieved.format(seller_id=format_seller_id)
+        
 def send_email_to_user(user_email, message, subject_retrieved):
-    from_email = "zactao.work@gmail.com"
+
+    from_email = "esdg9t1@gmail.com"
     to_emails = user_email
     subject = subject_retrieved
     plain_text_content = str(message)
     message = Mail(from_email, to_emails, subject, plain_text_content)
     try:
+  
         sg = SendGridAPIClient(SENDGRID_API_KEY)
+        print("hello this is invoked ")
         response = sg.send(message)
+        print("Email has been successfully sent")
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 401:
             print(
@@ -99,22 +129,7 @@ def send_email_to_user(user_email, message, subject_retrieved):
             )
         else:
             print("An error occurred:", e)
-    else:
-        print("Email has been successfully sent")
-
-
-def on_queue_callback(channel, method, properties, body):
-    try:
-        SUBJECT = "subject"
-        data = json.loads(body)
-        queue_name = "_".join(method.routing_key.split(".")[-2:])
-        subject = QUEUES[queue_name][SUBJECT]
-        channel.basic_ack(delivery_tag=method.delivery_tag)
-        # this is the data retrieved from the publisher
-        process_message(data, queue_name, subject)
-    except Exception as err:
-        logging.exception("Error processing message: %s", err)
-
+        
 
 if __name__ == "__main__":
     try:
@@ -127,10 +142,50 @@ if __name__ == "__main__":
                 exchangename=EXCHANGE,
                 exchangetype=EXCHANGE_TYPE,
                 queue=queue_name,
-                on_message_callback=on_queue_callback,
+                on_message_callback=callback,
             )
         channel.start_consuming()
-        print("consumed message on all queues")
     except KeyboardInterrupt:
         channel.stop_consuming()
         connection.close()
+
+
+# project_create
+# hello this is invoked 
+# Email has been successfully sent
+
+# ratings_reward
+# hello this is invoked 
+# Email has been successfully sent
+
+# ratings_penalise
+# hello this is invoked 
+# Email has been successfully sent
+
+# project_verify
+# hello this is invoked 
+# Email has been successfully sent
+
+# milestone_add
+# hello this is invoked 
+# Email has been successfully sent
+
+# payment_success
+# hello this is invoked 
+# Email has been successfully sent
+# hello this is invoked 
+# Email has been successfully sent
+
+# payment_failed
+# message is sent once
+# hello this is invoked 
+# Email has been successfully sent
+
+# rollbackpayment_failed
+# message is sent once
+# hello this is invoked 
+# Email has been successfully sent
+
+# milestone_upcoming
+# hello this is invoked 
+# Email has been successfully sent
--- notify/publisher.py
+++ notify/publisher.py
@@ -1,276 +1,70 @@
 import pika
 import json
-
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host="13.229.231.31", port=5672)
 )
 channel = connection.channel()
 
+with open('tests/milestone_add.json') as milestone_add:
+    milestone_add_data = json.load(milestone_add)
+
+with open('tests/milestone_upcoming.json') as milestone_upcoming:
+    milestone_upcoming_data = json.load(milestone_upcoming)
+
+with open('tests/milestone_penalise.json') as milestone_penalise:
+    milestone_penalise_data = json.load(milestone_penalise) 
+
+with open('tests/milestone_reward.json') as milestone_reward:
+    milestone_reward_data = json.load(milestone_reward)
+
+with open('tests/payment_failed.json') as payment_failed:
+    payment_failed_data = json.load(payment_failed)
+
+with open('tests/payment_success.json') as payment_success:
+    payment_success_data = json.load(payment_success)
+
+with open('tests/project_create.json') as project_create:
+    project_create_data = json.load(project_create)
+
+with open('tests/project_verify.json') as project_verify:
+    project_verify_data = json.load(project_verify)
+
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.projects.public.ratings.reward",
     # sent user email
-    body=json.dumps(
-        {
-            "resource_id": "ec2b5bc8-19d6-4a4f-a5e5-7b42b7d4851e",
-            "type": "milestone.reward",
-            "data": {
-                "id": "a3cc8e03-07e4-4211-a029-431beed4ef87",
-                "name": "100.5MW Wind Power Project in Madhya Pradesh, India",
-                "owner_id": "3b08a327-42b2-4694-bbeb-f61ad526ec47",
-                "description": "desc3",
-                "types": ["carbon capture", "local green initiative (small project)"],
-                "status": "verified",
-                "created_at": "2023-03-28T09:09:35Z",
-                "updated_at": "2023-03-28T09:10:57Z",
-                "rating": 110.0,
-                "milestones": {
-                    "id": "ec2b5bc8-19d6-4a4f-a5e5-7b42b7d4851e",
-                    "name": "0.5 million tCO2e saved by year 3",
-                    "description": "0.5 million tCO2e saved in 3 years after the launch of the windmill",
-                    "type": "Temporal",
-                    "offsets_available": 29.400000000000002,
-                    "offsets_total": 50.0,
-                    "status": "met",
-                    "created_at": "2023-03-28T09:09:35Z",
-                    "updated_at": "2023-03-28T09:18:58Z",
-                    "due_date": "2026-03-21T09:30:00Z",
-                    "project_id": "a3cc8e03-07e4-4211-a029-431beed4ef87",
-                },
-            },
-        }
-    ),
+    body=json.dumps(milestone_reward_data)
 )
 
-print(" Sent notify service the message on reward queue")
+print("Sent notify service the message on reward queue")
 
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.projects.public.ratings.penalise",
-    body=json.dumps(
-        {
-            "resource_id": "58a460e1-4276-4e25-8c42-520efaca56af",
-            "type": "milestone.penalise",
-            "data": {
-                "id": "a3cc8e03-07e4-4211-a029-431beed4ef87",
-                "name": "100.5MW Wind Power Project in Madhya Pradesh, India",
-                "owner_id": "3b08a327-42b2-4694-bbeb-f61ad526ec47",
-                "description": "desc3",
-                "types": ["carbon capture", "local green initiative (small project)"],
-                "status": "verified",
-                "created_at": "2023-03-28T09:09:35Z",
-                "updated_at": "2023-03-28T09:10:57Z",
-                "rating": 90.0,
-                "milestones": {
-                    "id": "58a460e1-4276-4e25-8c42-520efaca56af",
-                    "name": "Generation of 180 GWh of clean electricity annually",
-                    "description": "Generation of 180 GWh of clean electricity annually after the project has been implemented.",
-                    "type": "Temporal",
-                    "offsets_available": 50.0,
-                    "offsets_total": 50.0,
-                    "status": "rejected",
-                    "created_at": "2023-03-28T09:09:35Z",
-                    "updated_at": "2023-03-28T09:23:23Z",
-                    "due_date": "2024-03-21T19:30:22Z",
-                    "project_id": "a3cc8e03-07e4-4211-a029-431beed4ef87",
-                },
-            },
-        }
-    ),
+    body=json.dumps(milestone_penalise_data)
 )
 print(" Sent notify service the message on penalise queue")
 
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.projects.public.project.verify",
-    body=json.dumps(
-        {
-            "resource_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-            "type": "project.verify",
-            "data": {
-                "id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                "name": "100.5MW Wind Power Project in Madhya Pradesh, India",
-                "owner_id": "3b08a327-42b2-4694-bbeb-f61ad526ec47",
-                "description": "desc3",
-                "types": ["carbon capture", "local green initiative (small project)"],
-                "status": "Pending",
-                "created_at": "2023-03-26T19:10:30Z",
-                "updated_at": "2023-03-26T19:10:30Z",
-                "rating": 100.0,
-                "milestones": [
-                    {
-                        "id": "60af22a8-e44f-4300-95db-b776ae703848",
-                        "name": "0.5 million tCO2e saved by year 3",
-                        "description": "0.5 million tCO2e saved in 3 years after the launch of the windmill",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2026-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                    {
-                        "id": "4f5c1fe9-8187-4115-9e1e-c698751b998e",
-                        "name": "1 million tCO2e saved by year 6",
-                        "description": "1 million tCO2e saved by year 6 after the launch of the windmill",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2029-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                    {
-                        "id": "4119b4c1-b392-4e92-8e7a-801ebfdcebd3",
-                        "name": "Generation of 180 GWh of clean electricity annually",
-                        "description": "Generation of 180 GWh of clean electricity annually after the project has been implemented.",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2024-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                    {
-                        "id": "18d09220-bfed-4b98-874d-551eaa0af7f9",
-                        "name": "1.23 million tCO2e saved by year 7",
-                        "description": "1.23 million tCO2e saved by year 7 after the launch of the windmill",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2030-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                ],
-            },
-        }
-    ),
+    body=json.dumps(project_verify_data)
 )
 
-print(" Sent notify service the message on verify queue")
+print(" Sent notify service the message on project verify queue")
 
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.projects.public.milestone.add",
-    body=json.dumps(
-        {
-            "resource_id": "48d727ec-4d74-466c-bc3a-f0e566792250",
-            "type": "milestone.add",
-            "data": {
-                "id": "dd95aa97-f0b4-44c2-b069-22c3a4975f52",
-                "name": "100.5MW Wind Power Project in Madhya Pradesh, India",
-                "owner_id": "3b08a327-42b2-4694-bbeb-f61ad526ec47",
-                "description": "desc3",
-                "types": ["carbon capture", "local green initiative (small project)"],
-                "status": "pending",
-                "created_at": "2023-03-28T07:51:54Z",
-                "updated_at": "2023-03-28T07:51:54Z",
-                "rating": 100.0,
-                "milestones": [
-                    {
-                        "id": "48d727ec-4d74-466c-bc3a-f0e566792250",
-                        "name": "0.2 million tCO2e saved by year 2",
-                        "description": "0.2 million tCO2e saved by year 2 after project launch",
-                        "type": "Temporal",
-                        "offsets_available": 20.0,
-                        "offsets_total": 20.0,
-                        "status": "pending",
-                        "created_at": "2023-03-28T08:25:12Z",
-                        "updated_at": "2023-03-28T08:25:12Z",
-                        "due_date": "2025-03-21T09:31:23Z",
-                        "project_id": "dd95aa97-f0b4-44c2-b069-22c3a4975f52",
-                    }
-                ],
-            },
-        }
-    ),
+    body=json.dumps(milestone_add_data),
 )
 
-# print(" Sent notify service the message on milestone add queue")
+print(" Sent notify service the message on milestone add queue")
 
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.projects.public.project.create",
-    body=json.dumps(
-        {
-            "resource_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-            "type": "project.create",
-            "data": {
-                "id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                "name": "100.5MW Wind Power Project in Madhya Pradesh, India",
-                "owner_id": "3b08a327-42b2-4694-bbeb-f61ad526ec47",
-                "description": "desc3",
-                "types": ["carbon capture", "local green initiative (small project)"],
-                "status": "Pending",
-                "created_at": "2023-03-26T19:10:30Z",
-                "updated_at": "2023-03-26T19:10:30Z",
-                "rating": 100.0,
-                "milestones": [
-                    {
-                        "id": "60af22a8-e44f-4300-95db-b776ae703848",
-                        "name": "0.5 million tCO2e saved by year 3",
-                        "description": "0.5 million tCO2e saved in 3 years after the launch of the windmill",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2026-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                    {
-                        "id": "4f5c1fe9-8187-4115-9e1e-c698751b998e",
-                        "name": "1 million tCO2e saved by year 6",
-                        "description": "1 million tCO2e saved by year 6 after the launch of the windmill",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2029-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                    {
-                        "id": "4119b4c1-b392-4e92-8e7a-801ebfdcebd3",
-                        "name": "Generation of 180 GWh of clean electricity annually",
-                        "description": "Generation of 180 GWh of clean electricity annually after the project has been implemented.",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2024-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                    {
-                        "id": "18d09220-bfed-4b98-874d-551eaa0af7f9",
-                        "name": "1.23 million tCO2e saved by year 7",
-                        "description": "1.23 million tCO2e saved by year 7 after the launch of the windmill",
-                        "type": "Temporal",
-                        "offsets_available": 50.0,
-                        "offsets_total": 50.0,
-                        "status": "Pending",
-                        "created_at": "2023-03-26T19:10:30Z",
-                        "updated_at": "2023-03-26T19:10:30Z",
-                        "due_date": "2030-03-21T09:30:00Z",
-                        "project_id": "3a99ba93-6494-44fc-a3de-19bc5b9fbf6e",
-                    },
-                ],
-            },
-        }
-    ),
+    body=json.dumps(project_create_data),
 )
 
 print(" Sent notify service the message on project create queue")
@@ -278,37 +72,35 @@ print(" Sent notify service the message on project create queue")
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.police.notify.milestone.upcoming",
-    body=json.dumps(
-        {
-            "resource_id": "274487fhsdbchb-23ecsdv",
-            "type": "upcoming",
-            "data": {"project_id": "1", "milestone_id": "2"},
-        }
-    ),
+    body=json.dumps(milestone_upcoming_data),
 )
 
-print("Send message on milestone upcoming queue")
+print("Send notify service the message on milestone upcoming queue")
 
 
 channel.basic_publish(
     exchange="topic_exchange",
     routing_key="events.buyprojects.notify.payment.success",
-    body=json.dumps(
-        {
-            "resource_id": "6fbbfd6e-e2bc-435b-a323-fcb6997a0353",
-            "type": "payment.failed",
-            "data": {
-                "created_at": "2023-03-16 07:45:00",
-                "payment_id": "6fbbfd6e-e2bc-435b-a323-fcb6997a0353",
-                "milestone_id": "123",
-                "user_id": "456",
-                "amount": 10.0,
-            },
-        }
-    ),
+    body=json.dumps(payment_success_data),
 )
 
-print("Send message on buy projects queue")
+print("Send notify service the message on success payment buyprojects queue")
+
+channel.basic_publish(
+    exchange="topic_exchange",
+    routing_key="events.buyprojects.notify.payment.failed",
+    body=json.dumps(payment_failed_data),
+)
+
+print("Send notify service the message on failed payment buyprojects queue")
+
+channel.basic_publish(
+    exchange="topic_exchange",
+    routing_key="events.buyprojects.public.payment.failed",
+    body=json.dumps(payment_failed_data),
+)
+
+print("Send notify service the message on failed payment rollback buyprojects queue")
 
 
 connection.close()
