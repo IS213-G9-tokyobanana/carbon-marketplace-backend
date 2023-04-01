d032a5244c8f50baaa38e70228b1203227c22b08
Jaylin Lee <68802506+Jaylin0312@users.noreply.github.com>
2023-04-01 22:25:56 +0800

    feat: scheduler supervisor (#16)
    * feat: skeleton for scheduler supervisor
    
    * feat: completed supervisor logic
    
    * feat: added docker and poetry
    
    * fix: reuse create connection logic
    
    * refactor: put env example in main folder
    
    * fix: added json dumps for payload msg
    
    * fix: change the sequence of basic ack
    
    * fix: readme
    
    * fix: change connection heartbeat to 30

---
+++ SchedulerSupervisor/README.md
@@ -0,0 +1,71 @@
+# Project Polica complex microservice
+
+This is the Schedular Supervisor complex microservice. It is a Flask microservice that uses RabbitMQ as the message broker.
+
+### Built With
+
+- Python
+- Poetry
+
+### Prerequisites
+
+1. Python 3.11 - https://www.python.org/downloads/
+2. Poetry 1.4.0 - https://python-poetry.org/docs/#installation
+3. Docker 20.10.8 - https://docs.docker.com/get-docker/
+4. Have a live RabbitMQ instance running. Environment file should be configured to attach to the RabbitMQ instance.
+
+### Initial Setup
+
+1.  Clone the repository
+
+```bash
+git clone
+```
+
+2.  Copy and rename the `.env.example` file to `.env`. Please remember to update the environment variables accordingly.
+
+```bash
+cp .env.example .env
+```
+
+3.  Change configuration in `config.py` file based on whether it is production or development environment.
+
+### Running the app
+
+Follow these steps to set up this complex microservice.
+
+1. Ensure docker server is running
+
+2. Build the docker container image
+
+```
+docker build -t supervisor .
+```
+
+3. Run the docker container
+
+```
+docker run -d supervisor
+```
+
+### Local Development
+
+For local development, configure the rabbitMQ hostname variables in the `.env` file to 'localhost'. Then, continue with the following steps after running the docker container.
+
+1. Create a virtual environment
+
+```
+poetry shell
+```
+
+2. Install dependencies
+
+```
+poetry install
+```
+
+3. Run the app
+
+```
+python3 supervisor.py
+```
---
+++ SchedulerSupervisor/supervisor.py
@@ -0,0 +1,70 @@
+import logging
+import json
+from config.config import (
+    EXCHANGE_NAME,
+    TASK_STATUS_BINDING_KEY,
+    QUEUE_NAME,
+    SCHEDULER_SUPERVISOR_SCHEDULER_ROUTING_KEY,
+)
+from amqp_setup import check_setup, publish_message, create_connection
+
+
+def callback(channel, method, properties, body):
+    data = json.loads(body)
+    check_status(data)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def consume_message(
+    connection,
+    channel,
+    exchangename,
+    queue_name,
+    routing_key,
+):
+    """This function in this module consumes a message (persistent) from the exchange with a routing key."""
+    check_setup(connection, channel)
+    try:
+        channel.queue_declare(queue=queue_name, durable=True)
+        channel.queue_bind(
+            exchange=exchangename,
+            queue=queue_name,
+            routing_key=routing_key,
+        )
+        channel.basic_consume(queue_name, callback)
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+    except Exception as err:
+        logging.exception("Error processing message: %s", err)
+
+
+def check_status(data: dict):
+    if data["success"] == "True":
+        print("Task completed successfully!")
+    else:
+        payload = data["data"]["resource"]
+        payload = json.dumps(payload)
+        publish_message(
+            connection=connection,
+            channel=channel,
+            exchangename=EXCHANGE_NAME,
+            routing_key=SCHEDULER_SUPERVISOR_SCHEDULER_ROUTING_KEY,
+            message=payload,
+        )
+
+
+if __name__ == "__main__":
+    print(
+        f"monitoring the exchange {EXCHANGE_NAME} on binding key {TASK_STATUS_BINDING_KEY} ..."
+    )
+    connection = create_connection()
+    channel = connection.channel()
+    consume_message(
+        connection=connection,
+        channel=channel,
+        exchangename=EXCHANGE_NAME,
+        queue_name=QUEUE_NAME,
+        routing_key=TASK_STATUS_BINDING_KEY,
+    )
