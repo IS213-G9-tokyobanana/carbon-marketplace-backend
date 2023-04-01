6689c4f7625fd5f1218b67c96d89a1285fddf639
Jaylin Lee <68802506+Jaylin0312@users.noreply.github.com>
2023-04-01 22:56:48 +0800

    feat: basic temporal workflow for project police (#15)
    * feat: added temporal workflow for scenario 3
    
    * fix: file paths for docker
    
    * fix: put routing key in config
    
    * fix: change queue name
    
    * feat: added data in result
    
    * fix: change paths of project ms
    
    * fix: payload for project ms
    
    * refactor: logic for temporal
    
    * fix: corrected variable names
    
    * refactor: new variable for buyer id
    
    * fix: change variables `resources` to `resource`
    
    * feat: changed test message to always true
    (for testing purpose)
    
    * refactor: refined readme file
    
    * fix: remove buyer id for testing purpose
    
    * fix: temporal id
    
    * refactor: fixed payload
    
    * fix: change connection heartbeat to 30
    
    * refactor: rename folder

--- project_police/police.py
+++ project_police/police.py
@@ -3,12 +3,14 @@ import pika
 import json
 from config.config import EXCHANGE_NAME, POLICE_NOTIFY_ROUTING_KEY, PROJECT_STATUS_URL
 
-def format_url(pid, mid):
-    return PROJECT_STATUS_URL.format(project_id=pid, milestone_id=mid)
+
+def format_url(url, pid, mid):
+    return url.format(project_id=pid, milestone_id=mid)
+
 
 # Function to format message before sending to Notifier / Project Microservice
 def format_message(resource_id, type, data):
-    new_data = {k:v for k,v in data.items() if k != "task_id"}
+    new_data = {k: v for k, v in data.items() if k != "task_id"}
     return json.dumps({"resource_id": resource_id, "type": type, "data": new_data})
 
 
@@ -23,22 +25,31 @@ def publish_to_notifier(message, channel):
         body=payload,
         properties=pika.BasicProperties(delivery_mode=2),
     )
+    return {
+        "success": True,
+        "data": {
+            "message": "message sent to Notifier",
+            "resource": message,
+        },
+    }
 
 
 # Function to send message to Project Microservice
 def send_to_projectms(message):
-    milestone_id = message["data"]["milestone_id"]
     project_id = message["data"]["project_id"]
-    payload = format_message(milestone_id, project_id, message["data"])
-    url = format_url(project_id, milestone_id)
+    milestone_id = message["data"]["milestone_id"]
+    url = format_url(PROJECT_STATUS_URL, project_id, milestone_id)
 
     # Send request to Project Microservice
     try:
-        result = requests.patch(url, json=payload)
+        result = requests.patch(url, data={"status": "rejected"})
         result.raise_for_status()
     except requests.exceptions.HTTPError as err:
         result = {
-            "code": 500,
-            "message": "invocation of service fails: " + url + ". " + str(err),
+            "success": False,
+            "data": {
+                "message": "invocation of service fails: " + url + ". " + str(err),
+                "resource": message,
+            },
         }
     return result
