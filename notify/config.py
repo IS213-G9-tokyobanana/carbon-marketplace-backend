0cbaf1cd6c38e2d25d17deeda9ea2716b4ee7875
ZheTao <zacharytao99@gmail.com>
2023-04-02 17:38:17 +0800

    notify refactored

---
+++ notify/config.py
@@ -0,0 +1,14 @@
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+RABBITMQ_HOSTNAME = getenv("RABBITMQ_HOSTNAME") or "13.229.231.31"
+RABBITMQ_PORT = getenv("RABBITMQ_PORT") or 5672
+RABBITMQ_USERNAME = getenv("RABBITMQ_USERNAME") or "guest"
+RABBITMQ_PASSWORD = getenv("RABBITMQ_PASSWORD") or "guest"
+EXCHANGE = getenv("EXCHANGE")
+EXCHANGE_TYPE = getenv("EXCHANGE_TYPE")
+MS_BASE_URL = getenv("MS_BASE_URL")
+VERIFIERS_EMAILS = getenv("VERIFIERS_EMAILS")
+SENDGRID_API_KEY=getenv("SENDGRID_API_KEY")
+SENDGRID_FROM_EMAIL= "esdg9t1@gmail.com"
+
