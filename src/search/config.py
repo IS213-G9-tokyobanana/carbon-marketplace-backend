20547938c0e9e29b8dda0bab2c15a9fa0f2c384c
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-04-06 11:09:15 +0800

    feat: add api key for meilisearch

--- src/search/config.py
+++ src/search/config.py
@@ -6,26 +6,27 @@ env = "production"
 TOPIC_EXCHANGE_NAME = "topic_exchange"
 # Binding Keys that scheduler has to listen to
 BINDING_KEYS = {
-    "search.milestone.add":"events.projects.*.milestone.add",
-    "search.project.verify":"events.projects.*.project.verify",
-    "search.offsets.reserve":"events.projects.*.offsets.reserve",
-    "search.offsets.rollback":"events.projects.*.offsets.rollback",
-    "search.rating.penalise":"events.projects.*.rating.penalise",
-    "search.rating.reward":"events.projects.*.rating.reward",
+    "search.milestone.add": "events.projects.*.milestone.add",
+    "search.project.verify": "events.projects.*.project.verify",
+    "search.offsets.reserve": "events.projects.*.offsets.reserve",
+    "search.offsets.rollback": "events.projects.*.offsets.rollback",
+    "search.rating.penalise": "events.projects.*.rating.penalise",
+    "search.rating.reward": "events.projects.*.rating.reward",
 }
 
 
 if env == "production":
     load_dotenv()
-    RMQHOSTNAME = os.getenv('rmqhostname')
-    RMQUSERNAME = os.getenv('rmqusername')
-    RMQPASSWORD = os.getenv('rmqpassword')
-    RMQPORT = os.getenv('rmqport')
-    MEILIBASEURL = os.getenv('MEILI')
+    RMQHOSTNAME = os.getenv("rmqhostname")
+    RMQUSERNAME = os.getenv("rmqusername")
+    RMQPASSWORD = os.getenv("rmqpassword")
+    RMQPORT = os.getenv("rmqport")
+    MEILIBASEURL = os.getenv("MEILI")
+    MASTER_KEY = os.getenv("MEILI_MASTER_KEY")
 else:
     RMQHOSTNAME = "host.docker.internal"
     RMQUSERNAME = "guest"
     RMQPASSWORD = "guest"
     RMQPORT = 5672
-    MEILIBASEURL = os.getenv('MEILI')
-    
+    MEILIBASEURL = os.getenv("MEILI")
+    MASTER_KEY = os.getenv("MEILI_MASTER_KEY")
